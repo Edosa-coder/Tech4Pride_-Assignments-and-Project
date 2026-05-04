f7c4581cea6cdc068ec35d9cbf7459efed0deb4d
Samuel Edosa <samuelakpede@gmail.com>
2026-05-04 21:47:40 +0100

    Add files via upload

---
+++ Bicycle.py
@@ -0,0 +1,13 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[1])
+print(bicycles[2])
+print(bicycles[3])
+
+print("The first bicycle I owned was a " + bicycles[0].title() + ".")
+print("The second bicycle I owned was a " + bicycles[1].title() + ".")
+print("The third bicycle I owned was a " + bicycles[2].title() + ".")
+print("The fourth bicycle I owned was a " + bicycles[3].title() + ".")
+
+print(bicycles[0].title())
