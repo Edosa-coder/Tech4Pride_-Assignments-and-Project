61bce4170ede5f96e551c7d5407771499cae97ad
Samuel Edosa <samuelakpede@gmail.com>
2026-05-06 14:46:11 +0100

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,11 @@
+year = int(input("Enter the year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("February has 29 days")
+else:
+    print("February has 28 days")
+
+
+
+    check_leap_year = (2000)
+
+  
