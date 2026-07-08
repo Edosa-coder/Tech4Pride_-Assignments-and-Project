a651301d6f9563d8f03f7dc64435c2fecaf19c88
Edosa-coder <samuelakpede@gmail.com>
2026-07-08 11:56:24 +0100

    .\display_summary.py

---
+++ CGPA/display_summary.py
@@ -0,0 +1,16 @@
+from file_handler import load_data
+from student import *
+
+def main():
+    data=load_data()
+    if not data:
+        data=get_student_info()
+    while True:
+        print("\n1.Add Semester\n2.View Summary\n3.Exit")
+        c=input("Choice: ")
+        if c=="1": add_semester(data)
+        elif c=="2": display_summary(data)
+        elif c=="3": break
+        else: print("Invalid choice.")
+if __name__=="__main__":
+    main()
