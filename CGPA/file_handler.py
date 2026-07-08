cba4bd02f929c331bc5f3a0a344d5ffbfd648b37
Edosa-coder <samuelakpede@gmail.com>
2026-07-08 11:52:28 +0100

    .\file_handler.py

---
+++ CGPA/file_handler.py
@@ -0,0 +1,9 @@
+import json
+DATA_FILE="student_data.json"
+def save_data(data):
+    with open(DATA_FILE,"w") as f: json.dump(data,f,indent=4)
+def load_data():
+    try:
+        with open(DATA_FILE) as f:return json.load(f)
+    except (FileNotFoundError,json.JSONDecodeError):
+        return None
